9dbf8a4f14fddd2894b6c7fcc5a239a31327842d
plaf2000 <plaf2000@gmail.com>
2022-03-25 03:46:43 +0100

    Cloud.

--- frontend/src/app/meteo/meteo.component.ts
+++ frontend/src/app/meteo/meteo.component.ts
@@ -17,7 +17,7 @@ export class MeteoComponent implements OnInit {
   }
 
   precipitation = "";
-  meteoIcon = "";
+  meteoIcon = "kom:cloud-large";
   temperature = '';
   location: string | undefined;
   isHolyday = true;
@@ -47,8 +47,8 @@ export class MeteoComponent implements OnInit {
     fetch(`/api/weather?date=${this.message?.dateTimeArr}&station=${this.message?.to}`)
       .then(r => r.json())
       .then(r => {
-        this.temperature = r.data[0].value;
         this.meteoIcon = this.meteoIcons[r.data[2].value];
+        this.temperature = r.data[0].value;
         this.precipitation = r.data[1].value;
         this.location = this.message?.to;
         if (parseFloat(this.precipitation) > 0.1) {
